59280dde47fa82f6f1a0d9508c6ae3eae1a8f6fa
Baily locatelli <bailylocatelli@gmail.com>
2023-06-28 22:46:52 -0700

    added working routes and handlers

--- public/js/create.js
+++ public/js/create.js
@@ -44,3 +44,23 @@ document
    document
   .querySelector('.project-list')
   .addEventListener('click', delButtonHandler);
+
+  const createBtnHandler = async (event) => {
+    if (event.target.hasAttribute("create-entry-btn")) {
+      const id = event.target.getAttribute("new-project-form");
+  
+      const response = await fetch(`/api/entries/${id}`, {
+        method: "DELETE",
+      });
+  
+      if (response.ok) {
+        document.location.replace("/profile");
+      } else {
+        alert("Failed to delete project");
+      }
+    }
+  };
+  
+  document
+    .querySelector(".create-entry-btn")
+    .addEventListener("click", createBtnHandler);
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,20 +1,6 @@
 
-const createBtnHandler = async (event) => {
-  if (event.target.hasAttribute("create-entry-btn")) {
-    const id = event.target.getAttribute("new-project-form");
-
-    const response = await fetch(`/api/entries/${id}`, {
-      method: "DELETE",
-    });
-
-    if (response.ok) {
-      document.location.replace("/profile");
-    } else {
-      alert("Failed to delete project");
-    }
-  }
-};
-
-document
-  .querySelector(".create-entry-btn")
-  .addEventListener("click", createBtnHandler);
+var showListBtn = document.querySelector(".create-entry-btn");
+  showListBtn.addEventListener("click", function() {
+    
+    window.location.href = "/new-entry";
+  });
