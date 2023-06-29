dd82dae45a6b388cd333b78428d2ccbfbdd0ae22
sethaphelps <sethaphelps@gmail.com>
2023-06-28 20:53:11 -0700

    create profile/pages

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,46 +1,19 @@
-const newFormHandler = async (event) => {
-  event.preventDefault();
-
-  const name = document.querySelector('#project-name').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
-
-  if (name && description) {
-    const response = await fetch(`/api/entries`, {
-      method: 'POST',
-      body: JSON.stringify({ name, description }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to create project');
-    }
-  }
-};
-
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
+const createBtnHandler = async (event) => {
+  if (event.target.hasAttribute("create-entry-btn")) {
+    const id = event.target.getAttribute("new-project-form");
 
     const response = await fetch(`/api/entries/${id}`, {
-      method: 'DELETE',
+      method: "DELETE",
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace("/profile");
     } else {
-      alert('Failed to delete project');
+      alert("Failed to delete project");
     }
   }
 };
 
 document
-  .querySelector('.new-project-form')
-  .addEventListener('submit', newFormHandler);
-
-document
-  .querySelector('.project-list')
-  .addEventListener('click', delButtonHandler);
+  .querySelector(".create-entry-btn")
+  .addEventListener("click", createBtnHandler);
